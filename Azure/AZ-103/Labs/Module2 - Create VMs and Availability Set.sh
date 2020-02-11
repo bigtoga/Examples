f6f252888698844f6ba074df904bfa54cf24a5e0
bigtoga <scott.whigham@gmail.com>
2020-02-11 07:21:32 -0600

    Update and rename Module2.sh to Module2 - Create VMs and Availability Set.sh

--- Azure/AZ-103/Labs/Module2 - Create VMs and Availability Set.sh
+++ Azure/AZ-103/Labs/Module2 - Create VMs and Availability Set.sh
@@ -1,3 +1,6 @@
+# https://github.com/MicrosoftLearning/AZ-103-MicrosoftAzureAdministrator/blob/master/Instructions/Labs/02a%20-%20Deploy%20and%20Manage%20Virtual%20Machines%20(az-100-03).md
+
+# In Git Bash, run with "sh Thisfile.sh"
 alias az=az.cmd 
 
 # Login!
