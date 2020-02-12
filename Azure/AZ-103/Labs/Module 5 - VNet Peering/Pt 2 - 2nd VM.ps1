376c671914ce753f3a51d7e6533f2f075c83e257
bigtoga <scott.whigham@gmail.com>
2020-02-12 13:09:18 -0600

    Update Pt 2 - 2nd VM.ps1

--- Azure/AZ-103/Labs/Module 5 - VNet Peering/Pt 2 - 2nd VM.ps1
+++ Azure/AZ-103/Labs/Module 5 - VNet Peering/Pt 2 - 2nd VM.ps1
@@ -8,7 +8,7 @@ $raw_deploy_file = "https://raw.githubusercontent.com/MicrosoftLearning/AZ-103-M
 $raw_params_file = "https://raw.githubusercontent.com/MicrosoftLearning/AZ-103-MicrosoftAzureAdministrator/master/Allfiles/Labfiles/Module_05/VNet_Peering_and_Service_Chaining/az-100-04_azuredeploy.parameters.json"
 
 # Use "TemplateUri" and "TemplateParameterUri" when you want to deploy from https
-New-AzResourceGroupDeployment -ResourceGroupName $rg1.ResourceGroupName -TemplateUri $raw_deploy_file `
+New-AzResourceGroupDeployment -ResourceGroupName $rg2.ResourceGroupName -TemplateUri $raw_deploy_file `
   -TemplateParameterUri $raw_params_file -AsJob
 
 # Azure CLI: az group deployment create -g arm-template --template-uri $raw_deploy_file --parameters $raw_params_file
