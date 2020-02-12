28fc03dd04fbd05ed7a48d8836129f0c0928301d
bigtoga <scott.whigham@gmail.com>
2020-02-12 14:59:58 -0600

    Update and rename Finders - things Azure should make easier to find.ps1 to Things Azure should make easier to find.ps1

--- Azure/Finders - things Azure should make easier to find.ps1
+++
@@ -1,4 +0,0 @@
-Get-AzComputeResourceSku | where {$_.Locations -icontains "eastus"} | Where-Object {($_.ResourceType -ilike "virtualMachines")}
-
-# Can you run SQL Database in a given region or sku?
-Get-AzSqlCapability -LocationName "eastus"
---
+++ Azure/Things Azure should make easier to find.ps1
@@ -0,0 +1,13 @@
+# List all resource providers
+Get-AzResourceProvider -ListAvailable | Select-Object ProviderNamespace, RegistrationState
+
+Get-AzComputeResourceSku | where {$_.Locations -icontains "eastus"} | Where-Object {($_.ResourceType -ilike "virtualMachines")}
+
+# Can you run SQL Database in a given region or sku?
+# https://docs.microsoft.com/en-us/powershell/module/az.sql/get-azsqlcapability?view=azps-3.4.0
+
+# What are the defaults?
+Get-AzSqlCapability -LocationName "eastus" -Defaults
+
+# 
+Get-AzSqlCapability -LocationName "eastus" -ServiceObjectiveName "S1"
