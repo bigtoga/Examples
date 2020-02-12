3fa4ddf4fbad5f1ae2b199f5c5b154acde21b990
bigtoga <scott.whigham@gmail.com>
2020-02-12 10:53:49 -0600

    Update and rename Module 4 - Azure DNS.ps1 to Module 4 - Azure DNS - Pt 1 - Public Domains.ps1

--- Azure/AZ-103/Labs/Module 4 - Azure DNS - Pt 1 - Public Domains.ps1
+++ Azure/AZ-103/Labs/Module 4 - Azure DNS - Pt 1 - Public Domains.ps1
@@ -1,3 +1,8 @@
+####################################################################
+#
+# Part 1: Public Domains
+# 
+####################################################################
 # If RDP to VM in Azure, get public ip:
 Invoke-RestMethod http://ipinfo.io/json | Select-Object -ExpandProperty IP
 
@@ -11,3 +16,4 @@ New-AzPublicIpAddress -ResourceGroupName $rg.ResourceGroupName -Sku Basic -Alloc
 # Go create the DNS entries
 
 nslookup mylabvmpip.scottwhigham.com ns1-02.azure-dns.com
+####################################################################
