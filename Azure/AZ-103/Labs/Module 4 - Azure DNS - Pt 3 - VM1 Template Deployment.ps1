e3a1d08946df579fa16bec755e8c464285f0a519
bigtoga <scott.whigham@gmail.com>
2020-02-12 11:16:31 -0600

    Update Module 4 - Azure DNS - Pt 3 - VM1 Template Deployment.ps1

--- Azure/AZ-103/Labs/Module 4 - Azure DNS - Pt 3 - VM1 Template Deployment.ps1
+++ Azure/AZ-103/Labs/Module 4 - Azure DNS - Pt 3 - VM1 Template Deployment.ps1
@@ -7,8 +7,8 @@ $rg2 = Get-AzResourceGroup -Name 'az1000402b-RG'
 $raw_deploy_file = "https://raw.githubusercontent.com/MicrosoftLearning/AZ-103-MicrosoftAzureAdministrator/master/Allfiles/Labfiles/Module_04/Configure_Azure_DNS/az-100-04b_01_azuredeploy.json"
 $raw_params_file = "https://raw.githubusercontent.com/MicrosoftLearning/AZ-103-MicrosoftAzureAdministrator/master/Allfiles/Labfiles/Module_04/Configure_Azure_DNS/az-100-04_azuredeploy.parameters.json"
 
-New-AzResourceGroupDeployment -ResourceGroupName $rg2.ResourceGroupName -TemplateFile $raw_deploy_file `
-  -TemplateParameterFile $raw_params_file -AsJob
+New-AzResourceGroupDeployment -ResourceGroupName $rg2.ResourceGroupName -TemplateUri $raw_deploy_file `
+  -TemplateParameterUri $raw_params_file -AsJob
 
 # Azure CLI: az group deployment create -g arm-template --template-uri $raw_deploy_file --parameters $raw_params_file
 
