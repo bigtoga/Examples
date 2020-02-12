5f3dd77daf7051eae4d37977fabe6238bc769519
bigtoga <scott.whigham@gmail.com>
2020-02-12 14:16:37 -0600

    Update and rename Pt 2 - Configure Network Watcher.ps1 to Pt 2a - Configure VNET Peering.ps1

--- Azure/AZ-103/Labs/Module 6 - Network Watcher/Pt 2a - Configure VNET Peering.ps1
+++ Azure/AZ-103/Labs/Module 6 - Network Watcher/Pt 2a - Configure VNET Peering.ps1
@@ -1,5 +1,19 @@
 $location = "eastus"
 $rg1 = Get-AzResourceGroup -Name 'az1010301b-RG' -Location $location
+$rg2 = Get-AzResourceGroup -Name 'az1010302b-RG' -Location $location
+
+$vnet_rg1 = Get-AzVirtualNetwork -Name "az1010301b-vnet1" -ResourceGroupName $rg1 
+$vnet_rg2 = Get-AzVirtualNetwork -Name "az1010302b-vnet1" -ResourceGroupName $rg2 
+
+# From vnet_rg1 side: 
+$peering_name_rg1 = "az1010301b-vnet1-to-az1010302b-vnet2"
+Add-AzVirtualNetworkPeering `
+  -Name $peering_name_rg1
+  -VirtualNetwork $vnet_rg1 `
+  -RemoteVirtualNetworkId $vnet_rg2.Id
+  
+# PeeringState is now "Initiated" and will stay that way until you pair from the other side
+Get-AzVirtualNetworkPeering -Name $peering_name_rg1 -VirtualNetwork $vnet_rg1  -ResourceGroupName $rg1
 
 # Powershell Documentation: https://docs.microsoft.com/en-us/powershell/module/az.network/?view=azps-3.4.0#network_watcher
 
