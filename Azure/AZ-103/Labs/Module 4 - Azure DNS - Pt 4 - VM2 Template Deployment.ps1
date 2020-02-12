0afacd4f78628fd62bee9f24744f6030ec4b6ed0
bigtoga <scott.whigham@gmail.com>
2020-02-12 11:21:50 -0600

    Update Module 4 - Azure DNS - Pt 4 - VM2 Template Deployment.ps1

--- Azure/AZ-103/Labs/Module 4 - Azure DNS - Pt 4 - VM2 Template Deployment.ps1
+++ Azure/AZ-103/Labs/Module 4 - Azure DNS - Pt 4 - VM2 Template Deployment.ps1
@@ -5,7 +5,8 @@
 $raw_deploy_file = "https://raw.githubusercontent.com/MicrosoftLearning/AZ-103-MicrosoftAzureAdministrator/master/Allfiles/Labfiles/Module_04/Configure_Azure_DNS/az-100-04b_02_azuredeploy.json"
 $raw_params_file = "https://raw.githubusercontent.com/MicrosoftLearning/AZ-103-MicrosoftAzureAdministrator/master/Allfiles/Labfiles/Module_04/Configure_Azure_DNS/az-100-04_azuredeploy.parameters.json"
 
-New-AzResourceGroupDeployment -ResourceGroupName $rg2.ResourceGroupName -TemplateFile $ raw_deploy_file `
-  -TemplateParameterFile $raw_params_file -AsJob
+# Use "TemplateUri" and "TemplateParameterUri" when you want to deploy from https
+New-AzResourceGroupDeployment -ResourceGroupName $rg2.ResourceGroupName -TemplateUri $raw_deploy_file `
+  -TemplateParameterUri $raw_params_file -AsJob
   
 # Azure CLI: az group deployment create -g arm-template --template-uri $raw_deploy_file --parameters $raw_params_file
