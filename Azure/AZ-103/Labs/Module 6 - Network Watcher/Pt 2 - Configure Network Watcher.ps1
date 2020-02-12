834428bc3289185e4b9e38a0b331f1fce30accf2
bigtoga <scott.whigham@gmail.com>
2020-02-12 13:53:37 -0600

    Update Pt 2 - Configure Network Watcher.ps1

--- Azure/AZ-103/Labs/Module 6 - Network Watcher/Pt 2 - Configure Network Watcher.ps1
+++ Azure/AZ-103/Labs/Module 6 - Network Watcher/Pt 2 - Configure Network Watcher.ps1
@@ -4,4 +4,6 @@ $rg1 = Get-AzResourceGroup -Name 'az1010301b-RG' -Location $location
 # Powershell Documentation: https://docs.microsoft.com/en-us/powershell/module/az.network/?view=azps-3.4.0#network_watcher
 
 # List all network Watchers regardless of location, rg, or status
-Get-AzNetworkWatcher -Name NetworkWatcher*
+# Get-AzNetworkWatcher -Name NetworkWatcher*
+
+New-AzNetworkWatcher -Name NetworkWatcher_eastus -ResourceGroup NetworkWatcherRG -Location $location
