0ea43bdd4e23b0472f06fc408d907bcfb2d1c596
bigtoga <scott.whigham@gmail.com>
2020-02-12 14:00:32 -0600

    Update Pt 2 - Configure Network Watcher.ps1

--- Azure/AZ-103/Labs/Module 6 - Network Watcher/Pt 2 - Configure Network Watcher.ps1
+++ Azure/AZ-103/Labs/Module 6 - Network Watcher/Pt 2 - Configure Network Watcher.ps1
@@ -6,4 +6,9 @@ $rg1 = Get-AzResourceGroup -Name 'az1010301b-RG' -Location $location
 # List all network Watchers regardless of location, rg, or status
 # Get-AzNetworkWatcher -Name NetworkWatcher*
 
-New-AzNetworkWatcher -Name NetworkWatcher_eastus -ResourceGroup NetworkWatcherRG -Location $location
+# Not needed - Network Watcher is automatically enabled for a region when a vnet is created
+# New-AzNetworkWatcher -Name NetworkWatcher_eastus -ResourceGroup NetworkWatcherRG -Location $location
+# az network watcher configure --resource-group NetworkWatcherRG --locations westcentralus --enabled
+
+# To remove a Network Watcher:
+# Remove-AzNetworkWatcher -Name NetworkWatcher_westcentralus -ResourceGroup NetworkWatcherRG
