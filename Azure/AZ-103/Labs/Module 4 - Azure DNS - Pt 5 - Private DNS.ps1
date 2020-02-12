f372b16ea403bcc5932d09a1f07e2676a8f17133
bigtoga <scott.whigham@gmail.com>
2020-02-12 11:24:47 -0600

    Create Module 4 - Azure DNS - Pt 5 - Private DNS.ps1

---
+++ Azure/AZ-103/Labs/Module 4 - Azure DNS - Pt 5 - Private DNS.ps1
@@ -0,0 +1,10 @@
+# RDP to az1000402b-vm2 and run:
+
+nslookup az1000402b-vm1.adatum.corp
+
+# Create www entry
+New-AzPrivateDnsRecordSet -ResourceGroupName $rg2.ResourceGroupName -Name www -RecordType A `
+  -ZoneName adatum.corp -Ttl 3600 -PrivateDnsRecords (New-AzPrivateDnsRecordConfig -IPv4Address "10.104.0.4")
+
+# Verify it
+nslookup www.adatum.corp
