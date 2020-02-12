0bab0211a65ade5f611008cbc73de261b1b8d973
bigtoga <scott.whigham@gmail.com>
2020-02-12 10:44:07 -0600

    Create Module 4 - Azure DNS.ps1

---
+++ Azure/AZ-103/Labs/Module 4 - Azure DNS.ps1
@@ -0,0 +1,9 @@
+# If RDP to VM in Azure, get public ip:
+Invoke-RestMethod http://ipinfo.io/json | Select-Object -ExpandProperty IP
+
+# If you want to retrieve the public IP using powershell of a specific VM:
+az vm nic list --vm-name myVM --resource-group az1000401b-RG
+
+$rg = Get-AzResourceGroup -Name az1000401b-RG
+
+New-AzPublicIpAddress -ResourceGroupName $rg.ResourceGroupName -Sku Basic -AllocationMethod Static -Name az1000401b-pip -Location $rg.Location
