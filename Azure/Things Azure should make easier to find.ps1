2477f1da45ee62b2b5f7c50741ec87922a8484b3
bigtoga <scott.whigham@gmail.com>
2020-02-12 15:00:27 -0600

    Update Things Azure should make easier to find.ps1

--- Azure/Things Azure should make easier to find.ps1
+++ Azure/Things Azure should make easier to find.ps1
@@ -1,6 +1,10 @@
 # List all resource providers
 Get-AzResourceProvider -ListAvailable | Select-Object ProviderNamespace, RegistrationState
 
+# Want to add something to your subscription? 
+# Register-AzResourceProvider -ProviderNamespace Microsoft.Batch
+
+
 Get-AzComputeResourceSku | where {$_.Locations -icontains "eastus"} | Where-Object {($_.ResourceType -ilike "virtualMachines")}
 
 # Can you run SQL Database in a given region or sku?
