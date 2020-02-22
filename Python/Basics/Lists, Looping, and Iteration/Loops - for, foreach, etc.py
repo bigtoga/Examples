09f447166af1e823db69e38d5d387aa699574e7d
bigtoga <scott.whigham@gmail.com>
2020-02-22 12:00:09 -0600

    Update and rename Loops - for, foreach, etc.md to Loops - for, foreach, etc.py

--- Python/Basics/Lists, Looping, and Iteration/Loops - for, foreach, etc.py
+++ Python/Basics/Lists, Looping, and Iteration/Loops - for, foreach, etc.py
@@ -1,9 +1,8 @@
-~~~
 # Print the number of films that they were in
-x = response_json["films"]\
-dacount = 0\
-for y in x:\
+x = response_json["films"]
+dacount = 0
+for y in x:
     dacount+=1 
 
 print(f'There are {dacount} files')
-~~~
+
