32e513c7e765a32847960d975568e5940bf27636
bigtoga <scott.whigham@gmail.com>
2020-02-27 14:24:15 -0600

    Update Calendar - generate all dates in range.py

--- Python/Pandas/Calendar - generate all dates in range.py
+++ Python/Pandas/Calendar - generate all dates in range.py
@@ -5,3 +5,7 @@ start_date = dt.datetime(2020, 2, 15)
 end_date = dt.datetime(2021, 2, 15)
 
 pd.date_range(start_date, end_date, freq='W')
+
+# Can also do w periods
+
+pd.date_range("2017-01-01", periods=2)
