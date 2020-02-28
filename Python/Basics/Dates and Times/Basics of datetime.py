c78cb85664c2eb05fdc7346bb932bbee4765546c
bigtoga <scott.whigham@gmail.com>
2020-02-27 20:52:07 -0600

    Create Basics of datetime.py

---
+++ Python/Basics/Dates and Times/Basics of datetime.py
@@ -0,0 +1,4 @@
+# Source data: <span class="article-item__date" data-date="2020-02-27T00:00:00-0500"></span>
+datetime = "2020-02-27T00:00:00-0500"
+
+date = datetime.split('T')[0] # split at the "T" and get the element before it
