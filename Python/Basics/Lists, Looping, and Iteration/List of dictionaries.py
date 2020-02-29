e84c91cc0e0a5b8ff9d9f93e4d9ff17c3881789c
bigtoga <scott.whigham@gmail.com>
2020-02-29 12:35:32 -0600

    Create List of dictionaries.py

---
+++ Python/Basics/Lists, Looping, and Iteration/List of dictionaries.py
@@ -0,0 +1,28 @@
+
+    [
+        {
+            'type': 'Jessica',
+            'cost': '4',
+            'stock': 4
+        },
+        {
+            'type': 'Mark',
+            'cost': '5',
+            'stock':45
+        },
+        {
+            'type': 'Mark1',
+            'cost': '6',
+            'stock':45
+        },
+        {
+            'type': 'Mark2',
+            'cost': '4.1',
+            'stock':45
+        },
+        {
+            'type': 'Mark18',
+            'cost': '2',
+            'stock':45
+        }
+    ]
