848ed27df6619c266603bbad14d9560fc66e4594
Scott Whigham <scott.whigham@gmail.com>
2020-03-19 19:15:32 -0500

    Create Load API w JSON.js

---
+++ d3/Load API w JSON.js
@@ -0,0 +1,10 @@
+const url = "https://api.spacexdata.com/v2/launchpads";
+
+// Fetch the JSON data and console log it
+d3.json(url).then(function(data) {
+  console.log(data);
+});
+
+// Promise Pending
+const dataPromise = d3.json(url);
+console.log("Data Promise: ", dataPromise);
