e9d1617f601727f5c4544b6f0f67b103a0262d6d
Scott Whigham <scott.whigham@gmail.com>
2020-04-07 05:16:42 -0500

    Create Array of objects.js

---
+++ Javascript/Objects and Arrays/Array of objects.js
@@ -0,0 +1,25 @@
+// From Scott Murray's great tutorial 
+// https://alignedleft.com/tutorials/d3/data-types
+
+var fruits = [
+    {
+        kind: "grape",
+        color: "red",
+        quantity: 12,
+        tasty: true
+    },
+    {
+        kind: "kiwi",
+        color: "brown",
+        quantity: 98,
+        tasty: true
+    },
+    {
+        kind: "banana",
+        color: "yellow",
+        quantity: 0,
+        tasty: true
+    }
+];
+
+fruits[1].quantity  // Returns 98
