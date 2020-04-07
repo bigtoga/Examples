fde065f5e8797a1a80b6f55f69a567491ed29b50
Scott Whigham <scott.whigham@gmail.com>
2020-04-07 05:19:58 -0500

    Create Basic bar chart in d3.js

---
+++ Javascript/d3/Charting/Bar charts/Basic bar chart in d3.js
@@ -0,0 +1,15 @@
+// Source: Scott Murray's amazing tutorial 
+// https://alignedleft.com/tutorials/d3/making-a-bar-chart
+
+var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
+                11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
+
+d3.select("body").selectAll("div")
+    .data(dataset)
+    .enter()
+    .append("div")
+    .attr("class", "bar")
+    .style("height", function(d) {
+        var barHeight = d * 5;
+        return barHeight + "px";
+    });
