c15e31ce92db41da1fce369f266ea4e1e0148bac
Scott Whigham <scott.whigham@gmail.com>
2020-04-28 18:42:21 -0500

    Create PDFs - combine all into one PDF.py

---
+++ Python/PDFs - combine all into one PDF.py
@@ -0,0 +1,27 @@
+# Originally from "Automate the Boring Stuff" - https://automatetheboringstuff.com/
+
+import PyPDF2, os
+
+# Get all the PDF filenames.
+pdfFiles = []
+for filename in os.listdir('.'):
+    if filename.endswith('.pdf'):
+        pdfFiles.append(filename)
+pdfFiles.sort()
+
+pdfWriter = PyPDF2.PdfFileWriter()
+
+# Loop through all the PDF files.
+for filename in pdfFiles:
+    pdfFileObj = open(filename, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+    # Loop through all the pages (except the first) and add them.
+    for pageNum in range(1, pdfReader.numPages):
+        pageObj = pdfReader.getPage(pageNum)
+        pdfWriter.addPage(pageObj)
+
+# Save the resulting PDF to a file.
+pdfOutput = open('allminutes.pdf', 'wb')
+pdfWriter.write(pdfOutput)
+pdfOutput.close()
