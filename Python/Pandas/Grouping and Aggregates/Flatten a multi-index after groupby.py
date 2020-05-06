c28b6b50d207b8ec9f92eb973127b42f1587c7ba
Scott Whigham <scott.whigham@gmail.com>
2020-05-06 08:03:46 -0500

    Update Flatten a multi-index after groupby.py

--- Python/Pandas/Grouping and Aggregates/Flatten a multi-index after groupby.py
+++ Python/Pandas/Grouping and Aggregates/Flatten a multi-index after groupby.py
@@ -1,3 +1,5 @@
+# Multiple ways to do this
+
 # GroupBy object:
 gbAggs = df_clean.groupby(['Country/Region', 'Date']).agg({
         'Confirmed': [
@@ -13,3 +15,23 @@ gbAggs.columns = ["Country", "Date", "ConfirmedCases"];
 
 # Now you have a single index dataframe
 gbAggs.head()
+
+########################################################
+# Pandas also has .get_level_values() to allow you to address/get a specific axis' values
+# https://riptutorial.com/pandas/example/13285/select-from-multiindex-by-level
+df = pd.DataFrame(np.random.randn(6, 3), columns=['A', 'B', 'C'])
+df.set_index(['A', 'B'], inplace=True)
+
+# Can get by column name
+df.index.get_level_values('A')
+
+# or by index level
+df.index.get_level_values(level=0)
+
+# And even fancier
+df.loc[(df.index.get_level_values('A') > 0.5) & (df.index.get_level_values('A') < 2.1)]
+
+# To extract a specific value you can use xs (cross-section)
+df.xs(key=0.9027639999999999)
+
+df.xs(key=0.9027639999999999, drop_level=False)
