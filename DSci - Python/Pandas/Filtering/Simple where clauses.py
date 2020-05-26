9b4e44c69ca9ddee89e3592aa278fda53767c289
Scott Whigham <scott.whigham@gmail.com>
2020-05-26 12:49:28 -0500

    Update Simple where clauses.py

--- DSci - Python/Pandas/Filtering/Simple where clauses.py
+++ DSci - Python/Pandas/Filtering/Simple where clauses.py
@@ -1,2 +1,7 @@
 # Single column
 df[df["type"] == "Confirmed"]
+
+# Multiple filters
+
+dfOptimized = dfOptimized[(dfOptimized['origYear'] == 2011) | (dfOptimized['origYear'] == 2012)]
+dfOptimized.head()
