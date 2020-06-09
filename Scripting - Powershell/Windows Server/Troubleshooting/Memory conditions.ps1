fae49fd52537c2bc9aa03278d3c87d2427160727
Scott Whigham <scott.whigham@gmail.com>
2020-06-09 07:49:18 -0500

    Create Memory conditions.ps1

---
+++ Scripting - Powershell/Windows Server/Troubleshooting/Memory conditions.ps1
@@ -0,0 +1,4 @@
+# https://docs.microsoft.com/en-us/uwp/api/windows.system.appresourcegroupinfo.getmemoryreport?view=winrt-19041
+# Provides information about memory usage such as the commit limit, commit usage, and so on.
+
+Get-MemoryReport -Device <device name>
