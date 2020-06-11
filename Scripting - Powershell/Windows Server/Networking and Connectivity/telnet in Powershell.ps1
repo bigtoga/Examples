818355d2d23e95f0054abc4b645d1dd2c77c6684
Scott Whigham <scott.whigham@gmail.com>
2020-06-11 07:38:35 -0500

    Create telnet in Powershell.ps1

---
+++ Scripting - Powershell/Windows Server/Networking and Connectivity/telnet in Powershell.ps1
@@ -0,0 +1,28 @@
+# If you do not have telnet, multiple Powershell alternatives
+
+# Option 1: Use "Echo"
+Echo ((new-object Net.Sockets.TcpClient).Client.Connect('my_azure_sql_server_db_url', 1433)) 'success'
+
+# Option 2: Use .NET 
+$remote_host = 'my_azure_sql_server_db_url'
+$port = 1433
+
+$telnet = new-object Net.Sockets.TcpClient
+
+try{
+    $telnet.Client.Connect($remote_host, $port)
+
+    if($telnet.Connected){
+        Write-Host "Connected to $remote_host on port $port successfully!" -ForegroundColor Green
+        $telnet.Close()
+    }
+    else{
+        Write-Host "Unable to connect to $remote_host on port $port" -ForegroundColor Yellow
+    }
+}
+catch{
+    Write-Host "Error!" -ForegroundColor Red
+}
+finally{
+    $telnet.Dispose()
+}
