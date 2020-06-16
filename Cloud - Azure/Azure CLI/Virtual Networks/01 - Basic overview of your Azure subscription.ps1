47b313287fc0c3e7263d7007ac5fc2cc9fc0a4e4
Scott Whigham <scott.whigham@gmail.com>
2020-06-16 08:43:44 -0500

    Update 01 - Basic overview of your Azure subscription.ps1

--- Cloud - Azure/Azure CLI/Virtual Networks/01 - Basic overview of your Azure subscription.ps1
+++ Cloud - Azure/Azure CLI/Virtual Networks/01 - Basic overview of your Azure subscription.ps1
@@ -4,4 +4,7 @@
 az network vnet list
 
 # Sometimes that doesn't work - be more specific
-az network vnet list --subscription "My subscription" --query "[].[name]" -o tsv
+$subscription = "My subscription"
+az network vnet list --subscription $subscription --query "[].[name]" -o tsv
+
+az network vnet list --subscription $subscription --query "[0].{vnet:name, cidr_Range:addressSpace.addressPrefixes[0]}" -o table
