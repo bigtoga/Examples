63b09fd8caa736bad7668f3bb5490288e7b204f0
Scott Whigham <scott.whigham@gmail.com>
2020-09-28 15:19:13 -0500

    Create Bash commands.sh

---
+++ Cloud - Azure/AZ-500/Labs/Lab 01 - RBAC/Bash commands.sh
@@ -0,0 +1,19 @@
+# Execute in Azure Cloud Shell
+
+DOMAINNAME=$(az ad signed-in-user show --query 'userPrincipalName' | cut -d '@' -f 2 | sed 's/\"//')
+
+az ad user create --display-name "Dylan Williams" --password "Pa55w.rd1234" --user-principal-name Dylan@$DOMAINNAME
+
+az ad user list --output table
+
+#########################################################
+az ad group create --display-name "Service Desk" --mail-nickname "ServiceDesk"
+
+USER=$(az ad user list --filter "displayname eq 'Dylan Williams'")
+
+# 
+OBJECTID=$(echo $USER | jq '.[].objectId' | tr -d '"')
+
+az ad group member add --group "Service Desk" --member-id $OBJECTID
+
+az ad group member list --group "Service Desk"
