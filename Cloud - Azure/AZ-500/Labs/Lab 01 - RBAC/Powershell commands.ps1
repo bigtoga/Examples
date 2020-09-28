041d5bffe5346028f2842aa869be4c452f2c2b93
Scott Whigham <scott.whigham@gmail.com>
2020-09-28 15:13:23 -0500

    Create Powershell commands.ps1

---
+++ Cloud - Azure/AZ-500/Labs/Lab 01 - RBAC/Powershell commands.ps1
@@ -0,0 +1,28 @@
+# Use Azure Cloud Shell
+
+$passwordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+$passwordProfile.Password = 'Pa55w.rd1234'
+
+Connect-AzureAD
+$domainName = ((Get-AzureAdTenantDetail).VerifiedDomains)[0].Name
+
+###################################################################################
+# Create the new user
+
+New-AzureADUser -DisplayName 'Isabel Garcia' -PasswordProfile $passwordProfile -UserPrincipalName "Isabel@$domainName" -AccountEnabled $true -MailNickName 'Isabel'
+
+Get-AzureADUser 
+
+###################################################################################
+# Create the new group
+
+New-AzureADGroup -DisplayName 'Junior Admins' -MailEnabled $false -SecurityEnabled $true -MailNickName JuniorAdmins
+
+Get-AzureADGroup
+
+$user = Get-AzureADUser -Filter "MailNickName eq 'Isabel'"
+
+Add-AzADGroupMember -MemberUserPrincipalName $user.userPrincipalName -TargetGroupDisplayName "Junior Admins" 
+
+Get-AzADGroupMember -GroupDisplayName "Junior Admins"
+
