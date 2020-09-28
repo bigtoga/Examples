80e46959ca8e8cf5110046b04bc3cacfa8528a5a
Scott Whigham <scott.whigham@gmail.com>
2020-09-28 15:52:34 -0500

    Update Powershell commands.ps1

--- Cloud - Azure/AZ-500/Labs/Lab 03 - Locks/Powershell commands.ps1
+++ Cloud - Azure/AZ-500/Labs/Lab 03 - Locks/Powershell commands.ps1
@@ -2,3 +2,10 @@ New-AzResourceGroup -Name AZ500LAB03 -Location 'EastUS'
 
 New-AzStorageAccount -ResourceGroupName AZ500LAB03 -Name (Get-Random -Maximum 999999999999999) -Location  EastUS -SkuName Standard_LRS -Kind StorageV2 
 
+$storageaccountname = (Get-AzStorageAccount -ResourceGroupName AZ500LAB03).StorageAccountName
+
+$lockName = (Get-AzResourceLock -ResourceGroupName AZ500LAB03 -ResourceName $storageAccountName -ResourceType Microsoft.Storage/storageAccounts).Name
+
+Remove-AzResourceLock -LockName $lockName -ResourceName $storageAccountName  -ResourceGroupName AZ500LAB03 -ResourceType Microsoft.Storage/storageAccounts -Force
+
+
