f999bdd8fbefdc9c50d7409a29008be5c53b5941
Scott Whigham <scott.whigham@gmail.com>
2020-09-30 17:47:06 -0500

    Create Powershell commands.ps1

---
+++ Cloud - Azure/AZ-500/Labs/Lab 10 - Azure Key Vault and SQL/Exercise 2/Powershell commands.ps1
@@ -0,0 +1,6 @@
+# After creating the registered app + creating a client-side secret, allow access to AKV
+
+$applicationId = '50ac12d5-00b9-4633-b29b-3f89498ce5e2'
+$kvName = (Get-AzKeyVault -ResourceGroupName 'AZ500LAB10').VaultName
+
+Set-AZKeyVaultAccessPolicy -VaultName $kvName -ResourceGroupName AZ500LAB10 -ServicePrincipalName $applicationId -PermissionsToKeys get,wrapKey,unwrapKey,sign,verify,list
