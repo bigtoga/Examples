de66a71e1fa55d218ad86b4ffc760b986a58bccd
Scott Whigham <scott.whigham@gmail.com>
2020-09-30 18:18:59 -0500

    Update Powershell commands.ps1

--- Cloud - Azure/AZ-500/Labs/Lab 10 - Azure Key Vault and SQL/Exercise 2/Powershell commands.ps1
+++ Cloud - Azure/AZ-500/Labs/Lab 10 - Azure Key Vault and SQL/Exercise 2/Powershell commands.ps1
@@ -4,3 +4,6 @@ $applicationId = '50ac12d5-00b9-4633-b29b-3f89498ce5e2'
 $kvName = (Get-AzKeyVault -ResourceGroupName 'AZ500LAB10').VaultName
 
 Set-AZKeyVaultAccessPolicy -VaultName $kvName -ResourceGroupName AZ500LAB10 -ServicePrincipalName $applicationId -PermissionsToKeys get,wrapKey,unwrapKey,sign,verify,list
+
+Install-Package Microsoft.SqlServer.Management.AlwaysEncrypted.AzureKeyVaultProvider
+Install-Package Microsoft.IdentityModel.Clients.ActiveDirectory
