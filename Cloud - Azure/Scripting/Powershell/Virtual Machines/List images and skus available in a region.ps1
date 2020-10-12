3c6534f4e4139ffb7b45ba0c7d6e75b66caf865a
Scott Whigham <scott.whigham@gmail.com>
2020-10-12 09:25:46 -0500

    Update List images and skus available in a region.ps1

--- Cloud - Azure/Scripting/Powershell/Virtual Machines/List images and skus available in a region.ps1
+++ Cloud - Azure/Scripting/Powershell/Virtual Machines/List images and skus available in a region.ps1
@@ -10,3 +10,11 @@ $offerName = 'WindowsServer'
 Get-AzVMImageSku -Location $location -PublisherName $publisherName -Offer $offerName | Select Skus | Where {$_.Skus -like "2019*"}
 
 $skuName = '2019-Datacenter-gensecond'
+
+#########################################################################
+# Compute size
+
+$all_skus = Get-AzComputeResourceSku | Where {$_.Locations -icontains $location}
+$all_skus | Where {$_.ResourceType -eq "disks"}
+
+Get-AzVMSize -Location $location | Where {$_.Name -like "Standard*"}
