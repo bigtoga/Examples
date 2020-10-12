5b6289f58dc2df7531abb25077657949845cbc9b
Scott Whigham <scott.whigham@gmail.com>
2020-10-12 07:34:23 -0500

    Create Install Az Powershell module.ps1

---
+++ Cloud - Azure/Scripting/Powershell/Install Az Powershell module.ps1
@@ -0,0 +1,8 @@
+# https://docs.microsoft.com/en-us/powershell/azure/install-az-ps?view=azps-4.7.0
+
+if ($PSVersionTable.PSEdition -eq 'Desktop' -and (Get-Module -Name AzureRM -ListAvailable)) {
+    Write-Warning -Message ('Az module not installed. Having both the AzureRM and ' +
+      'Az modules installed at the same time is not supported.')
+} else {
+    Install-Module -Name Az -AllowClobber -Scope CurrentUser
+}
