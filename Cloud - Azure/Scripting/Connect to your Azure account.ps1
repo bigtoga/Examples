db107c38de2198f33986213ad2a550e1a31d24bb
Scott Whigham <scott.whigham@gmail.com>
2020-10-12 07:40:12 -0500

    Create Connect to your Azure account.ps1

---
+++ Cloud - Azure/Scripting/Connect to your Azure account.ps1
@@ -0,0 +1,9 @@
+Connect-AzAccount
+
+Get-AzTenant
+
+$subscription = (Get-AzSubscription | Where {$_.Name -eq "Azure Pass - Sponsorship"}).Id
+
+Set-AzContext -SubscriptionId $subscription
+
+$context = (Get-AzContext | Select -ExpandProperty Account)
