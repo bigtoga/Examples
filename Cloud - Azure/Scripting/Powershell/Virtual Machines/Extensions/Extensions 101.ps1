6b964f93ba75daef09bc66dcba6de8e59d0d9051
Scott Whigham <scott.whigham@gmail.com>
2020-10-13 09:54:52 -0500

    Create Extensions 101.ps1

---
+++ Cloud - Azure/Scripting/Powershell/Virtual Machines/Extensions/Extensions 101.ps1
@@ -0,0 +1,18 @@
+
+clear-host
+
+# What publishers are available to you? 
+Get-AzVMImagePublisher -Location "SouthCentralUS" | Where {$_.PublisherName -like "Microsoft*" -and $_.PublisherName -notlike "Test*" -and $_.Id -notlike "Test*"} 
+
+# What extensions does that publisher make?
+Get-AzVMExtensionImageType -PublisherName "Microsoft.Azure.Security" -Location "SouthCentralUS" | Get-AzVMExtensionImage | Select Type, Version, PublisherName
+
+# What versions of the extension are there?
+# Note - most of the version numbers are Major.Minor in the -TypeHandlerVersion but might be listed as 1.5.5.9
+Get-AzVMExtensionImage -Type IaaSAntimalware -PublisherName "Microsoft.Azure.Security" -Location "SouthCentralUS" 
+
+# Enable it
+
+SettingsString = "{ ""AntimalwareEnabled"": true }"
+Set-AzVMExtension -ResourceGroupName "foobar" -Location "SouthCentralUS" -VMName "MyVM1" `
+  -Name "IaaSAntimalware" -Publisher "Microsoft.Azure.Security" -Type "IaaSAntimalware" -TypeHandlerVersion "1.5" -SettingString $SettingsString
