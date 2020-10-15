4f2eb44214dbbf62302df49c1981117a37804629
Scott Whigham <scott.whigham@gmail.com>
2020-10-15 07:32:52 -0500

    Update Install Az Powershell module.ps1

--- Cloud - Azure/Scripting/Powershell/Install Az Powershell module.ps1
+++ Cloud - Azure/Scripting/Powershell/Install Az Powershell module.ps1
@@ -6,3 +6,15 @@ if ($PSVersionTable.PSEdition -eq 'Desktop' -and (Get-Module -Name AzureRM -List
 } else {
     Install-Module -Name Az -AllowClobber -Scope CurrentUser
 }
+
+
+# If you get errors about NuGet or PowershellGet
+#     1. Close Powershell
+#     2. Launch as Administrator
+#     3. Make sure you are using TLS 1.2
+
+# Configure to use TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Install Azure module
+Install-Module -Name Az -AllowClobber  -Force
