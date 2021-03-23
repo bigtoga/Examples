eb54a3388950cbae9fdd9834b7eea04de343f5eb
Scott Whigham <scott.whigham@gmail.com>
2021-03-23 10:59:14 -0500

    Create List all IPs in a subnet.ps1

---
+++ Scripting - Powershell/Windows Server/DNS/List all IPs in a subnet.ps1
@@ -0,0 +1,41 @@
+Clear-Host
+
+##############################################
+# When running locally on DC / DNS server
+
+# Requires Run as Administrator permission
+##############################################
+
+# List all DNS zones for the DNS server
+Get-DnsServerZone | Format-Table
+
+# For-Each
+$Zones = @(Get-DnsServerZone)
+ForEach ($Zone in $Zones) {
+	Write-Host "`n$($Zone.ZoneName)" -ForegroundColor "Green"
+	$Zone | Get-DnsServerResourceRecord
+}
+
+# List only a specific zone's A records
+Get-DnsServerResourceRecord -ZoneName local.app | Where-Object {$_.RecordType -eq "A"}
+
+# List only a specific zone's A records that start with DEV
+Get-DnsServerResourceRecord -ZoneName local.app | Where-Object {$_.RecordType -eq "A" -and $_.HostName -like "DEV*"}
+
+# List only a specific zone's C names
+Get-DnsServerResourceRecord -ZoneName local.app | Where-Object {$_.RecordType -eq "CName"}
+
+# WMI local't any better though 
+Get-WmiObject -Namespace Root\MicrosoftDNS -Query "SELECT * FROM MicrosoftDNS_AType WHERE ContainerName='local.app'" | Format-Table
+
+
+
+##############################################
+# When running remotely
+##############################################
+$DNSServer = "servernameOrIp"
+$Zones = @(Get-DnsServerZone -ComputerName $DNSServer)
+ForEach ($Zone in $Zones) {
+	Write-Host "`n$($Zone.ZoneName)" -ForegroundColor "Green"
+	$Zone | Get-DnsServerResourceRecord -ComputerName $DNSServer
+}
