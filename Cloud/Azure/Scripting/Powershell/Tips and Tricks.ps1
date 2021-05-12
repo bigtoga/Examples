eef7d8b32a4db2c2833b12903fb929a716fe1404
Scott Whigham <bigtoga@users.noreply.github.com>
2021-05-12 04:30:31 -0500

    tip

--- Cloud/Azure/Scripting/Powershell/Tips and Tricks.ps1
+++ Cloud/Azure/Scripting/Powershell/Tips and Tricks.ps1
@@ -1,2 +1,5 @@
 # Create a storage account w a unique name
 New-AzStorageAccount -ResourceGroupName AZ500LAB03 -Name (Get-Random -Maximum 999999999999999) -Location  EastUS -SkuName Standard_LRS -Kind StorageV2
+
+# Set the default respurce group name to save time
+Set-AzDefault -ResourceGroupName [sandbox resource group name]
