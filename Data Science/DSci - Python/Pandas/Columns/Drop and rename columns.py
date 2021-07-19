23809207802f7d59752b6e815daf6cca224a0bd4
Scott Whigham <scott.whigham@gmail.com>
2021-07-19 08:45:55 -0500

    Update Drop and rename columns.py

--- Data Science/DSci - Python/Pandas/Columns/Drop and rename columns.py
+++ Data Science/DSci - Python/Pandas/Columns/Drop and rename columns.py
@@ -5,6 +5,8 @@ df = df.drop(df.columns[[0, 1, 2, 4, 5]], axis=1)
 # *Note: If you don’t find what you need here, check out the pandas cookbook
 # https://pandas.pydata.org/pandas-docs/stable/user_guide/cookbook.html
 
-dfChild = dfTemp[(dfTemp["Work Item Type"] == "Task")]
-dfChild = dfChild.drop(columns = ["Title 1", "State", "Work Item Type", "Tags"])
+# Rename by name:
 dfChild.rename(columns = {'Title 2':'Task'}, inplace = True)
+
+# Rename by index: 
+dfChild.rename(columns = {0:"Tag", 1:"Tags"}, inplace = True)
