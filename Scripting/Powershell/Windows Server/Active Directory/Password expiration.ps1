b47b351c60a6376601f47aaadd364e92868ea72f
Scott Whigham <scott.whigham@gmail.com>
2023-01-23 06:54:13 -0600

    Update Password expiration.ps1

--- Scripting/Powershell/Windows Server/Active Directory/Password expiration.ps1
+++ Scripting/Powershell/Windows Server/Active Directory/Password expiration.ps1
@@ -1,4 +1,4 @@
 Get-ADUser -filter {Enabled -eq $True -and PasswordNeverExpires -eq $False} `
     –Properties DisplayName, msDS-UserPasswordExpiryTimeComputed | `
-    Select-Object -Property Displayname,@{Name=ExpiryDate;Expression={[datetime]::FromFileTime($_.msDS-UserPasswordExpiryTimeComputed)}} | `
+    Select-Object -Property Displayname, @{Name=ExpiryDate;Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}} | `
     Sort-Object displayname # | export-csv h:\passwords_list.csv
