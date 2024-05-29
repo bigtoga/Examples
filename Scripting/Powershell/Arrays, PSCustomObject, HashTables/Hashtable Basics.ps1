e031e5fe653f0e7ba0a60299d2e6a54f8484256b
Scott Whigham <scott.whigham@gmail.com>
2024-05-29 07:27:17 -0500

    Update Hashtable Basics.ps1

--- Scripting/Powershell/Arrays, PSCustomObject, HashTables/Hashtable Basics.ps1
+++ Scripting/Powershell/Arrays, PSCustomObject, HashTables/Hashtable Basics.ps1
@@ -1,6 +1,14 @@
 # Create, use
+$hash = $null
+$hash = @{}
 
+$proc = get-process | Sort-Object -Property name -Unique
+ 
+foreach ($p in $proc){
+    $hash.add($p.name,$p.id)
+}
 
+$hash
 
 ##################################
 # Update
